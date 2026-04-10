af4fc7b00eb45c2230045c269bdab6bbc811d9cc
Harul Arora <harularora463@gmail.com>
2026-04-11 01:54:04 +0530

    Use CLIENT_URL for CORS whitelist

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://questdeck.onrender.com',
-        process.env.FRONTEND_URL?.replace(/\/$/, '')
+        process.env.CLIENT_URL?.replace(/\/$/, '')
     ].filter(Boolean),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
